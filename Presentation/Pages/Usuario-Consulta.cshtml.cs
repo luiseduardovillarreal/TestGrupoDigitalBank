1009d3d01023e76cf5a40a22759adf4ab846ac05
Luis Eduardo Villarreal Tobío <luedvito1994@gmail.com>
2026-02-17 23:03:40 -0500

    Inicialización de proyecto.

---
+++ Presentation/Pages/Usuario-Consulta.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Presentacion.Pages
+{
+    public class Usuario_ConsultaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
